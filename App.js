dc93cd0a4fbd86fb566930650f54a05538bd0e7b
Jared Webber <jaredwebberdev@gmail.com>
2023-01-02 16:58:10 -0600

    update session date when required (#85)
    * update session date when required
    
    * version bump
    
    * version bump
    
    * handle different date formats

--- App.js
+++ App.js
@@ -3,10 +3,11 @@
  * @flow strict-local
  */
 
-import React, {useEffect} from 'react';
+import React, {useRef, useEffect} from 'react';
+import {useGlobalStore} from './store/activity_store';
+import {AppState} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import RootNavigator from './components/scenes/root_navigator';
-import {useGlobalStore} from './store/activity_store';
 
 const App = () => {
 	const initialLoad = false;
@@ -15,6 +16,27 @@ const App = () => {
 	useEffect(() => {
 		store.populateStore();
 	}, [initialLoad]);
+
+	const appState = useRef(AppState.currentState);
+
+	useEffect(() => {
+		const subscription = AppState.addEventListener('change', nextAppState => {
+			if (
+				appState.current.match(/inactive|background/) &&
+				nextAppState === 'active'
+			) {
+				console.log('App has come to the foreground!');
+				store.tryUpdatePeriod();
+			}
+
+			appState.current = nextAppState;
+			console.log('AppState', appState.current);
+		});
+
+		return () => {
+			subscription.remove();
+		};
+	}, []);
 
 	return (
 		<GestureHandlerRootView style={{flex: 1}}>
